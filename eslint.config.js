423b869275d7875bd045aa351a0999eab8858d97
Nicholas Zivkovic <znichola@student.42nice.fr>
2025-04-09 10:44:39 +0200

    install prettier and cofigure with esling
    using this doc : https://prettier.io/docs/install
    
    Do not run prettier! until it's merged to main, this will make life much
    simpelr later.

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 export default [
   { ignores: ['dist'] },
@@ -30,4 +31,5 @@ export default [
       ],
     },
   },
+  eslintConfigPrettier
 ]
