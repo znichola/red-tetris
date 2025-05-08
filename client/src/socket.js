f5e7ec283480accf7b8783edebc13b5c37d26011
Vasco Vandenbosch <vasco.vandenbosch@fishingcactus.com>
2025-05-08 09:35:12 +0200

    Revert "removed unused conditino in socket url"
    This reverts commit d1ada4ec9ac7a5a12b25d8ed6f6caf3e58921133.

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,7 @@
 import { io } from "socket.io-client";
 
-const URL = "http://localhost:3000";
+const URL =
+  process.env.NODE_ENV === "production" ? undefined : "http://localhost:3000";
 
 export const socket = io(URL, {
   transports: ["websocket"],
